514cf2f3c6431caa0d1dab3b0ddc2582b74eb6a6
immaculatecapper <markkokhan@gmail.com>
2025-03-05 12:31:49 -0500

    D: added player

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from player import Player
+
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
 
 def main():
     pygame.init()
@@ -7,6 +11,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(x, y)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -17,6 +22,7 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player1.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
